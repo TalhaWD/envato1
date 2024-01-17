c233e1528d41bdce6e2cbec5d8050bf314f057bb
TalhaWD <you@example.com>
2024-01-17 21:35:08 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// function myprofile(){
+//     var pass = document.getElementById("pass").value;
+//     if(pass!=""){
+//         if(pass=="yahya"){
+//             window.open("");
+//         }else{
+//             alert("your password is wrong");
+//         }
+//        } else{
+//             alert("please fill the form");
+//         }
+//  
+gsap.to("#yah",{
+    x:300,
+    y:180,
+    duration:3,
+    delay:0.5,
+    color:"red",
+})
+gsap.to("#saj",{
+    x:350,
+    y:270,
+    duration:3,
+    delay:3,
+    color:"gray",
+    })
